f0b65ad4bbfc0357207a9fdc664e6aa98bc38da7
zhangwei <zhangwei@lichoin.com>
2026-01-04 18:21:45 +0800

    feat: add server module for unified lifecycle and server abstraction
    - Define `Server` interface to standardize various transport protocols.
    - Implement centralized orchestration for server startup and graceful shutdown.
    - Integrate Governor (internal diagnostics) and REST Server.

---
+++ pkg/server/types.go
@@ -0,0 +1,37 @@
+// Copyright 2022 The codesjoy Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"github.com/codesjoy/yggdrasil/pkg"
+)
+
+// Endpoint is the interface that wraps the Endpoint method.
+type Endpoint interface {
+	Scheme() string
+	Address() string
+	Metadata() map[string]string
+	Kind() pkg.ServerKind
+}
+
+// Server is the interface that wraps the Serve method.
+type Server interface {
+	RegisterService(sd *ServiceDesc, ss interface{})
+	RegisterRestService(sd *RestServiceDesc, ss interface{}, prefix ...string)
+	RegisterRestRawHandlers(sd ...*RestRawHandlerDesc)
+	Serve(startFlag chan<- struct{}) error
+	Stop() error
+	Endpoints() []Endpoint
+}
