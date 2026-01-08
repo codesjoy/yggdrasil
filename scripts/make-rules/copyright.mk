053bd0ca376ab8eb22569b6c317e59deeec9d9b8
justfine <bracken.codesjoy@gmail.com>
2026-01-08 19:13:57 +0800

    build: fix glob patterns for addlicense ignore

--- scripts/make-rules/copyright.mk
+++ scripts/make-rules/copyright.mk
@@ -6,12 +6,12 @@
 copyright.verify: tools.verify.addlicense
 	@$(LOG_INFO) "Verifying the boilerplate headers for all files"
 	@addlicense --check -f $(ROOT_DIR)/scripts/boilerplate.txt \
-		-ignore vendor/** -ignore third_party/** -ignore _output/** -ignore **/*.yaml \
+		-ignore "vendor/**" -ignore "third_party/**" -ignore "_output/**" -ignore "**/*.yaml" -ignore "**/.*/**" \
 		$(ROOT_DIR)
 
 .PHONY: copyright.add
 copyright.add: tools.verify.addlicense
 	@$(LOG_INFO) "Add the boilerplate headers for all files"
 	@addlicense -v -f $(ROOT_DIR)/scripts/boilerplate.txt \
-		-ignore vendor/** -ignore third_party/** -ignore _output/** -ignore **/*.yaml \
-		$(ROOT_DIR)
+		-ignore "vendor/**" -ignore "third_party/**" -ignore "_output/**" -ignore "**/*.yaml" -ignore "**/.*/**" \
+		$(ROOT_DIR) 2>&1 | grep -v -E "skipping|already has"  || true
